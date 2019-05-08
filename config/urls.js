85a67b0e9ea6a97432a6bfb338b237cdc85b3d15
bodeaux <ploissken@gmail.com>
2019-05-07 23:51:52 -0300

    adding emojis as development status; i like to be weird

--- config/urls.js
+++ config/urls.js
@@ -1,7 +1,7 @@
 module.exports = [
-  { name: 'jornalistas livres', path: '/j-livres', url: 'https://jornalistaslivres.org/' },
-  { name: 'midia ninja', path: '/ninjas', url: 'http://midianinja.org/' },
+  { name: 'jornalistas livres 🔗', path: '/j-livres', url: 'https://jornalistaslivres.org/' },
+  { name: 'midia ninja ', path: '/ninjas', url: 'http://midianinja.org/' },
   { name: 'observatorio da imprensa', path: '/obs-imp', url: 'http://observatoriodaimprensa.com.br/' },
-  { name: 'diario do centro do mundo', path: '/dcm', url: 'https://www.diariodocentrodomundo.com.br/' },
-  { name: 'the intercept brasil', path: '/intercept', url: 'https://theintercept.com/brasil/' }
+  { name: 'diario do centro do mundo 🔗 🌠', path: '/dcm', url: 'https://www.diariodocentrodomundo.com.br/' },
+  { name: 'the intercept brasil 🔗', path: '/intercept', url: 'https://theintercept.com/brasil/' }
 ]
