98c34d600697cdceb2744cdeac8518e4961b6b4b
bodeaux <ploissken@gmail.com>
2019-05-08 12:15:15 -0300

    remove duplicates, merge imgs and links with title

--- src/parser/dcm.js
+++ src/parser/dcm.js
@@ -1,19 +1,27 @@
 module.exports = {
-  parse(ret) {
+  parse (ret) {
     let $ = ret.$
     let urls = $('a')
-    // TODO: map img and link to the same title
-    let arr = []
+
+    // remove duplicates && map img and link to same title
+    let uniqueList = {}
     for (var i = 0; i < urls.length; i++) {
       if (urls[i].attribs.href && urls[i].attribs.title && !urls[i].attribs.class) {
+        uniqueList[urls[i].attribs.title] = uniqueList[urls[i].attribs.title] || {}
         let imgs = urls[i].children.find(ch => ch.name === 'img')
         if (imgs) {
-          arr.push(`<a href=${imgs.attribs.src}>img</a> ${imgs.attribs.title}`)
+          uniqueList[imgs.attribs.title].img = imgs.attribs.src
         } else {
-          arr.push(`<a href=${urls[i].attribs.href}>link</a> ${urls[i].attribs.title}`)
+          uniqueList[urls[i].attribs.title].href = urls[i].attribs.href
         }
       }
     }
+
+    // compose response
+    let arr = []
+    Object.keys(uniqueList).forEach((key, index) => {
+      arr.push(`<a href=${uniqueList[key].href}>link</a> <a href=${uniqueList[key].img}>img</a> ${key}`)
+    })
     return arr
   }
 }
