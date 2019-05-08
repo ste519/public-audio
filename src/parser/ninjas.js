c6af4340af33b87c465490289571bbdf7362831e
bodeaux <ploissken@gmail.com>
2019-05-08 10:09:17 -0300

    all the links now properly crawled; imgs still missing

--- src/parser/ninjas.js
+++ src/parser/ninjas.js
@@ -1,5 +1,16 @@
 module.exports = {
   parse(ret) {
-    return ['we need to do this yet']
+    let $ = ret.$
+    let urls = $('a')
+    // TODO: grab and map imgs
+    let arr = []
+    for (var i = 0; i < urls.length; i++) {
+      if (urls[i].attribs.href &&
+        urls[i].children[0].data.length > 15) { // pretty ugly workaround
+        console.log(urls[i])
+        arr.push(`<a href=${urls[i].attribs.href}>link</a> ${urls[i].children[0].data}`)
+      }
+    }
+    return arr
   }
 }
--- src/parser/obs-imp.js
+++ src/parser/obs-imp.js
@@ -1,5 +1,19 @@
 module.exports = {
   parse(ret) {
-    return ['we need to do this yet']
+    let $ = ret.$
+    let high = $('h1[class=\"titulo\"]')
+    let urls = $('h2')
+    high = high[0].children[0]
+
+    // TODO: grab and map imgs
+    let arr = []
+    arr.push(`<a href=${high.attribs.href}>link</a> ${high.children[0].data}`)
+    for (var i = 0; i < urls.length; i++) {
+      if (urls[i]) {
+        var link = urls[i].children[0]
+        arr.push(`<a href=${link.attribs.href}>link</a> ${link.children[0].data}`)
+      }
+    }
+    return arr
   }
 }
